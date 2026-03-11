873714ab6d3c9564990ef0bdfb13af28fde8d014
joaovsmp <jvsmpsilva@furb.br>
2026-03-11 20:37:20 -0300

    Add files via upload

---
+++ EnunciadoA.java
@@ -0,0 +1,16 @@
+public class EnunciadoA {
+  public static void main(String[] args) {
+ 
+    // Entrada
+    int valor1 = 8;
+    int valor2 = 7;
+
+    // Processo
+    int soma = (valor1 + valor2 );
+
+    // Saída
+    System.out.println("Soma: " + soma);
+    
+  }
+  
+}
